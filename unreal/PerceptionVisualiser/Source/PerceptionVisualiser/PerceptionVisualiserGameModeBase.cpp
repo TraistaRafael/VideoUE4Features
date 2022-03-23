3b2c6543814f47245cbb169abc7160f382d1b00c
TraistaRafael <traista.rafael@gmail.com>
2022-03-23 18:29:58 +0200

    UE4 project base

---
+++ unreal/PerceptionVisualiser/Source/PerceptionVisualiser/PerceptionVisualiserGameModeBase.cpp
@@ -0,0 +1,10 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "PerceptionVisualiserGameModeBase.h"
+
+APerceptionVisualiserGameModeBase::APerceptionVisualiserGameModeBase(const FObjectInitializer& ObjectIn) : AGameModeBase(ObjectIn)
+{
+	UE_LOG(LogTemp, Display, TEXT("APerceptionVisualiserGameModeBase Constructor"));
+}
+
